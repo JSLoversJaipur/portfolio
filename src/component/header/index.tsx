eba1695ed0986d8ddc03b436349a4f6d39472328
Janit Yadav <janityadav16@gmail.com>
2024-07-10 21:56:05 +0530

    tailwind practice

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,42 +1,20 @@
 import React from 'react';
 import './header.css';
 import config from '../../config'
-function Header(){
-return (
-  <header className="bg-danger text-light fixed-top">
-  <div className="container">
-      <div className="row align-items-center">
-          <div className="col-md-4">
-              <h1 className="logo">{config.information.firstname}</h1>
-          </div>
-          <div className="col-sm-8">
-              <nav className="navbar  navbar-expand-md navbar-dark">
-                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                      <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div className="collapse navbar-collapse text-right" id="navbarNav">
-                      <ul className="navbar-nav ml-auto text-right">
-                          <li className="nav-item">
-                              <a className="nav-link" href="#aboutme">About</a>
-                          </li>
-                          <li className="nav-item">
-                              <a className="nav-link" href="#education">Education</a>
-                          </li>
-                          <li className="nav-item">
-                              <a className="nav-link" href="#project">Project</a>
-                          </li>
-                          <li className="nav-item">
-                              <a className="nav-link" href="#gallery">Gallery</a>
-                          </li>
+function Header() {
+    return (
+        <header className="header bg-danger text-light fixed top-0">
+            <div className="container mx-auto px-4">
+                <div className="logo text-3xl font-bold text-left">Harry</div>
 
-                      </ul>
-                  </div>
-              </nav>
-          </div>
-      </div>
-  </div>
-</header>
-);
+                <nav className="nav flex justify-end items-center item-center text-right">
+                    <a href="#aboutme" className="nav-link mr-4 text-white item-center text-right">About</a>
+                    <a href="#education" className="nav-link mr-4 text-white text-right">Education</a>
+                    <a href="#project" className="nav-link mr-4 text-white text-right">Project</a>
+                    <a href="#gallery" className="nav-link text-white text-right">Gallery</a>
+                </nav>
+            </div>
+        </header>
+    );
 }
 export default Header;
