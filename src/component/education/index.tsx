bfcde6d004bfc86386681053c3c7a8874de8cce4
Janit Yadav <janityadav16@gmail.com>
2024-07-12 13:50:18 +0530

    bootstrap to tailwind

--- src/component/education/index.tsx
+++ src/component/education/index.tsx
@@ -1,94 +1,94 @@
 import React from "react";
 import './education.css';
 import config from '../../config'
-function Education(){
-    return(
+function Education() {
+    return (
         <div className="education" id="education">
-        <div className="row">
-            <div className="col-lg-8 mx-auto">
-                <h2 className="text-center">Education</h2>
+            <div className="row">
+                <div className="col-lg-8 mx-auto my-auto">
+                    <h2 className="text-center">Education</h2>
 
 
-                {config.education.map((value:any,index:any)=>{
-                    
-                    return(
-                        <>
-                        <p className="text-decoration-none"><a href={value.url}
-                        >{value.name}</a></p>
-                <p>{value.discription}</p>
-                </>
-                    )
+                    {config.education.map((value: any, index: any) => {
 
-        
-                })}
-                
-                
+                        return (
+                            <>
+                                <p className="text-decoration-none"><a href={value.url}
+                                >{value.name}</a></p>
+                                <p>{value.discription}</p>
+                            </>
+                        )
 
 
-
-                <h2 className="text-center">certificate</h2>
-                {config.certificate.map((value:any,index:any)=>{
-                    
-                    return(
-                        <>
-                        <p className="text-decoration-none"><a href={value.url}
-                        >{value.name}</a></p>
-                <p>{value.discription}</p>
-                </>
-                    )
-
-        
-                })}
-
-
-
-
-                <h2 className="text-center m-5">Confrenses and meetups</h2>
-                {config.confrenses.map((value:any,index:any)=>{
-                    
-                    return(
-                        <>
-                        <p className="text-decoration-none"><a href={value.url}
-                        >{value.name}</a></p>
-                <p>{value.discription}</p>
-                </>
-                    )
-
-        
-                })}
-
-<h2 className="text-center m-5">Projects</h2>
-                {config.project.map((value:any,index:any)=>{
-                    
-                    return(
-                        <>
-                        <p className="text-decoration-none"><a href={value.url}
-                        >{value.name}</a></p>
-                <p>{value.discription}</p>
-                </>
-                    )
-
-        
-                })}
+                    })}
 
 
 
 
 
-                <h2 className="text-center">Github Contribution</h2>
-                <div className="text-center">
-                    <a href={"https://github.com/"+config.information.github}>
-                        <img src={"https://ghchart.rshah.org/"+config.information.github} alt="2016rshah's Github chart"/>
-                    </a>
-     
+                    <h2 className="text-center">certificate</h2>
+                    {config.certificate.map((value: any, index: any) => {
+
+                        return (
+                            <>
+                                <p className="text-decoration-none"><a href={value.url}
+                                >{value.name}</a></p>
+                                <p>{value.discription}</p>
+                            </>
+                        )
+
+
+                    })}
+
+
+
+
+                    <h2 className="text-center m-5">Confrenses and meetups</h2>
+                    {config.confrenses.map((value: any, index: any) => {
+
+                        return (
+                            <>
+                                <p className="text-decoration-none"><a href={value.url}
+                                >{value.name}</a></p>
+                                <p>{value.discription}</p>
+                            </>
+                        )
+
+
+                    })}
+
+                    <h2 className="text-center m-5">Projects</h2>
+                    {config.project.map((value: any, index: any) => {
+
+                        return (
+                            <>
+                                <p className="text-decoration-none"><a href={value.url}
+                                >{value.name}</a></p>
+                                <p>{value.discription}</p>
+                            </>
+                        )
+
+
+                    })}
+
+
+
+
+
+                    <h2 className="text-center">Github Contribution</h2>
+                    <div className="text-center">
+                        <a href={"https://github.com/" + config.information.github}>
+                            <img src={"https://ghchart.rshah.org/" + config.information.github} alt="2016rshah's Github chart" />
+                        </a>
+
+                    </div>
+
                 </div>
-
             </div>
         </div>
-    </div>
- 
+
     )
 
-    
+
 }
 export default Education
