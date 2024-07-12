fc68e8e5081712f4364c1e5f95fa21d1c1051767
Janit Yadav <janityadav16@gmail.com>
2024-07-12 13:46:47 +0530

    bootstrap to tailwind

--- src/component/about/index.tsx
+++ src/component/about/index.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import config from '../../config';
-function About(){
-    console.log(config.discription,"===");
-    return(
-<div className="About" id="About">
-    <div className="jumbotron">
+function About() {
+    console.log(config.discription, "===");
+    return (
+        <div className="About" id="About">
+            <div className="jumbotron">
 
-        <div className="col-lg-10 mx-auto">
-            <h2 className="text-center">About Us</h2>
-            <p className="text-justify" style = {{
-              fontWeight: 500,
-              fontSize: "18px"
-            }}>
-             {config.discription}   
-            </p>
+                <div className="col-lg-10 mx-auto my-auto">
+                    <h2 className="text-center">About Us</h2>
+                    <p className="text-justify" style={{
+                        fontWeight: 500,
+                        fontSize: "18px"
+                    }}>
+                        {config.discription}
+                    </p>
+                </div>
+
+            </div>
         </div>
-
-    </div>
-</div>
 
     )
 }
